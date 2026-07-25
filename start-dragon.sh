e06b53d3eb92640363f844ad5d83c088c466c78b
Robert Swirsky <robert@thrillscience.com>
2026-07-24 18:18:57 -0700

    Dragon start script

--- start-dragon.sh
+++ start-dragon.sh
@@ -1,2 +1,2 @@
-./build/iridium-sniffer -i usrp-B200-3229C91 --position --web=9090 --usrp-gain=65 --clock-source=external --time-source=external --acars
+ ./build/iridium-sniffer -i usrp-B200-3229C91 --usrp-gain=65     --clock-source=external --time-source=external     --save-bursts ./bursts --start-next-minute -t 60
 
