ceaeb3e450510c0c7b487be68ac6ce0f3adae485
Robert Swirsky <robert@thrillscience.com>
2026-07-31 13:08:59 -0700

    Clear out both "d" and "r" directory at run start

--- start-dragon.sh
+++ start-dragon.sh
@@ -1,4 +1,5 @@
 rm -rf d
+rm -rf r
 
 ./build/iridium-sniffer -i usrp-B200-3229C91 --usrp-gain=65     --clock-source=external --time-source=external     --save-bursts ./d --start-next-minute -t 10 
 
