edb6788c46f2d0ca8ac863e10904454e9fbddf17
swirsky <swirsky@gmail.com>
2026-07-31 12:26:07 -0700

    Start script for "r" Z

--- start-reuven.sh
+++ start-reuven.sh
@@ -1,1 +1,2 @@
-./build/iridium-sniffer -i usrp-B200-321E16D  --usrp-gain=65     --clock-source=external --time-source=external     --save-bursts ./bursts --start-next-minute -t 60
+rm -rf r
+./build/iridium-sniffer -i usrp-B200-321E16D  --usrp-gain=65     --clock-source=external --time-source=external     --save-bursts ./r --start-next-minute -t 10 
